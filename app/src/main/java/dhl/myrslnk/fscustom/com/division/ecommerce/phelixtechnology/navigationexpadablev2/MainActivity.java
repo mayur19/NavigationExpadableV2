da5e05a8b2ae06a055753d8fa01712a3d2936762
Mayur Dusane <mayurdusane1@gmail.com>
2017-11-28 20:18:20 +0530

    Initial commit

---
+++ app/src/main/java/dhl/myrslnk/fscustom/com/division/ecommerce/phelixtechnology/navigationexpadablev2/MainActivity.java
@@ -0,0 +1,13 @@
+package dhl.myrslnk.fscustom.com.division.ecommerce.phelixtechnology.navigationexpadablev2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
